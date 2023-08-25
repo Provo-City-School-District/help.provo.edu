3cfcd79c53d3d260084f86fe93cb763f09b54f89
Josh Espinoza <joshespi@gmail.com>
2023-08-25 11:32:38 -0600

    fixed connection to local database swtiching to mysqli instead of pdo

--- src/includes/helpdbconnect.php
+++ src/includes/helpdbconnect.php
@@ -1,18 +1,3 @@
 <?php
-function connect_help_db() {
-    $DATABASE_HOST = getenv('HELPMYSQL_HOST');
-    $DATABASE_USER = getenv('HELPMYSQL_USER');
-    $DATABASE_PASS = getenv('HELPMYSQL_PASSWORD');
-    $DATABASE_NAME = getenv('HELPMYSQL_DATABASE');
-    $DATABASE_PORT = getenv('HELPMYSQL_PORT');
-
-    try {
-        $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';port=' . $DATABASE_PORT . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-    } catch (PDOException $e) {
-        echo 'Failed to connect to database! ' . $e->getMessage();
-        exit();
-    }
-}
-
-$database = connect_help_db();
+$database = mysqli_connect(getenv("HELPMYSQL_HOST"), getenv("HELPMYSQL_USER"), getenv("HELPMYSQL_PASSWORD"), getenv("HELPMYSQL_DATABASE")) or die('failed to connect');
 ?>

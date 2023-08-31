c93ba520044be24973efbafe613059106a5b3728
Josh Espinoza <joshespi@gmail.com>
2023-08-31 10:22:50 -0600

    modified localdb connection to give an error when it cant connect

--- src/includes/helpdbconnect.php
+++ src/includes/helpdbconnect.php
@@ -1,3 +1,6 @@
 <?php
-$database = mysqli_connect(getenv("HELPMYSQL_HOST"), getenv("HELPMYSQL_USER"), getenv("HELPMYSQL_PASSWORD"), getenv("HELPMYSQL_DATABASE")) or die('failed to connect');
+$database = mysqli_connect(getenv("HELPMYSQL_HOST"), getenv("HELPMYSQL_USER"), getenv("HELPMYSQL_PASSWORD"), getenv("HELPMYSQL_DATABASE"));
+if (!$database) {
+    die('Could not connect to MySQL: ' . mysqli_connect_error());
+}
 ?>

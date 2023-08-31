033046e9842c87bfd5b72ebd312123cd53e1a4c9
Josh Espinoza <joshespi@gmail.com>
2023-08-31 10:47:20 -0600

    simple check for permissions. with a is_admin permission that bypasses the other permission

--- src/tickets.php
+++ src/tickets.php
@@ -1,4 +1,16 @@
-<?php include("includes/header.php"); ?>
+<?php 
+include("includes/header.php"); 
+
+if ($_SESSION['permissions']['is_admin'] != 1) {
+    // User is not an admin
+    if ($_SESSION['permissions']['can_view_tickets'] == 0) {
+        // User does not have permission to view tickets
+        echo 'You do not have permission to view tickets.';
+        exit;
+    }
+}
+
+?>
 
 <h1> Tickets Page</h1>
 

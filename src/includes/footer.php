cfdc9ad4ffd68947c31935a56e0f9ad892ebb22b
Josh Espinoza <joshespi@gmail.com>
2023-09-07 12:54:35 -0600

    hide dev info

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -9,14 +9,14 @@
 
 
 <?php
-echo '<h3>POST</h3>';
-print_r($_POST);
-echo '<h3>GET</h3>';
-print_r($_GET);
-echo '<h3>SESSION</h3>';
-print_r($_SESSION);
-echo '<h3>FILES</h3>';
-print_r($_FILES);
+// echo '<h3>POST</h3>';
+// print_r($_POST);
+// echo '<h3>GET</h3>';
+// print_r($_GET);
+// echo '<h3>SESSION</h3>';
+// print_r($_SESSION);
+// echo '<h3>FILES</h3>';
+// print_r($_FILES);
 // echo '<h3>COOKIE</h3>';
 // print_r($_COOKIE);
 // echo '<h3>SERVER</h3>';

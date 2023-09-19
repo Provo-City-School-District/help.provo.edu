2d6ee7b225b9a34beaa2d64c6b8c87297e13726d
Josh Espinoza <joshespi@gmail.com>
2023-09-19 13:59:03 -0600

    created a simple create form and insert method for new tickets

---
+++ src/controllers/tickets/create_ticket.php
@@ -0,0 +1,44 @@
+<?php
+include("../../includes/header.php");
+
+if ($_SESSION['permissions']['is_admin'] != 1) {
+    // User is not an admin
+    if ($_SESSION['permissions']['can_create_tickets'] == 0) {
+        // User does not have permission to view tickets
+        echo 'You do not have permission to view tickets.';
+        exit;
+    }
+}
+// require_once('../../includes/helpdbconnect.php');
+
+?>
+<article id="ticketWrapper">
+    
+    <h1>Create Ticket</h1>
+    <!-- Form for updating ticket information -->
+    <form method="POST" action="insert_ticket.php">
+        
+        <label for="location">Location:</label>
+        <input type="text" id="location" name="location" value=""><br>
+
+        <label for="room">Room:</label>
+        <input type="text" id="room" name="room" value=""><br>
+
+        <label for="name">Ticket Title:</label>
+        <input type="text" id="name" name="name" value=""><br>
+
+        <label for="description">Ticket Description:</label>
+        <textarea id="description" name="description"></textarea><br>
+
+       
+
+
+
+ 
+
+        <!-- Add a submit button to update the information -->
+        <input type="submit" value="Create Ticket">
+    </form>
+</article>
+
+<?php include("../../includes/footer.php"); ?>
---
+++ src/controllers/tickets/insert_ticket.php
@@ -0,0 +1,48 @@
+<?php
+require_once('../../includes/init.php');
+require_once('../../includes/helpdbconnect.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Sanitize and validate user inputs
+    $location = filter_input(INPUT_POST, 'location', FILTER_SANITIZE_SPECIAL_CHARS);
+    $room = filter_input(INPUT_POST, 'room', FILTER_SANITIZE_SPECIAL_CHARS);
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    // Check if required fields are empty
+    if (empty($location) || empty($room) || empty($name) || empty($description)) {
+        // Handle empty fields (e.g., show an error message)
+        die('All fields are required');
+    }
+
+    // Create an SQL INSERT query
+    $insertQuery = "INSERT INTO tickets (location, room, name, description, created, last_updated, due_date, status)
+                VALUES (?, ?, ?, ?, NOW(), NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY), 'open')";
+
+    // Prepare the SQL statement
+    $stmt = mysqli_prepare($database, $insertQuery);
+
+    if ($stmt === false) {
+        die('Error preparing insert query: ' . mysqli_error($database));
+    }
+
+    // Bind parameters
+    mysqli_stmt_bind_param($stmt, 'ssss', $location, $room, $name, $description);
+
+    // Execute the prepared statement
+    if (mysqli_stmt_execute($stmt)) {
+        // After successfully inserting the ticket, set a success message;
+        $_SESSION['success_message'] = "Ticket created successfully.";
+        
+        // After successfully inserting the ticket, fetch the ID of the new ticket
+    $ticketId = mysqli_insert_id($database);
+    
+    // Redirect to the edit page for the new ticket
+    header('Location: edit_ticket.php?id=' . $ticketId);
+        exit;
+    } else {
+        // Handle the error (e.g., show an error message)
+        die('Error creating ticket: ' . mysqli_error($database));
+    }
+}
+?>

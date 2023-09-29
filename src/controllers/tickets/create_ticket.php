b420085a9aaf7e798df674b262b3c02bd1edbeaf
Josh Espinoza <joshespi@gmail.com>
2023-09-29 07:17:58 -0600

    added controlled location select to create ticket

--- src/controllers/tickets/create_ticket.php
+++ src/controllers/tickets/create_ticket.php
@@ -1,6 +1,6 @@
 <?php
 include("../../includes/header.php");
-
+require_once('../../includes/helpdbconnect.php');
 if ($_SESSION['permissions']['is_admin'] != 1) {
     // User is not an admin
     if ($_SESSION['permissions']['can_create_tickets'] == 0) {
@@ -27,7 +27,21 @@ if ($_SESSION['permissions']['is_admin'] != 1) {
     <form method="POST" action="insert_ticket.php" enctype="multipart/form-data">
         <input type="hidden" name="client" value="<?= $_SESSION['username'] ?>">
         <label for="location">Location:</label>
-        <input type="text" id="location" name="location" value="<?= isset($_GET['location']) ? htmlspecialchars($_GET['location']) : '' ?>"><br>
+        <select id="location" name="location">
+                    <?php
+                    // Query the sites table to get the site information
+                    $location_query = "SELECT sitenumber, location_name FROM locations";
+                    $location_result = mysqli_query($database, $location_query);
+                    // Loop through the results and create an option for each site
+                    while ($locations = mysqli_fetch_assoc($location_result)) {
+                        $selected = '';
+                        if ($locations['sitenumber'] == $row['location']) {
+                            $selected = 'selected';
+                        }
+                        echo '<option value="' . $locations['sitenumber'] . '" ' . $selected . '>' . $locations['location_name'] . '</option>';
+                    }
+                    ?>
+                </select>
 
         <label for="room">Room:</label>
         <input type="text" id="room" name="room" value="<?= isset($_GET['room']) ? htmlspecialchars($_GET['room']) : '' ?>"><br>

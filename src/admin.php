e26a9697aa776d4a7994cfd1e125df71b4b03da5
Josh Espinoza <joshespi@gmail.com>
2023-10-05 07:36:09 -0600

    updated ticket query variables

--- src/admin.php
+++ src/admin.php
@@ -69,47 +69,47 @@ if (!$user_result) {
         ORDER BY id ASC";
 
         $ticket_result = mysqli_query($database, $ticket_query);
-        while ($row = mysqli_fetch_assoc($ticket_result)) {
-            $last_update = date("y-m-d", strtotime($row['last_updated']));
-            $created = $row['created']; // Get the value from the database
+        while ($ticket_row = mysqli_fetch_assoc($ticket_result)) {
+            $last_update = date("y-m-d", strtotime($ticket_row['last_updated']));
+            $created = $ticket_row['created']; // Get the value from the database
 
             if ($created !== null) {
                 $created = date("y-m-d", strtotime($created)); // Convert to date if not null
             } else {
                 $created = ''; // Set to an empty string or handle the case as needed
             }
-            $due_date = date("y-m-d", strtotime($row['due_date']));
+            $due_date = date("y-m-d", strtotime($ticket_row['due_date']));
             $overdue = strtotime($due_date) < strtotime(date("Y-m-d"));
         ?>
             <tr>
-                <td data-cell="ID"><a href="/controllers/tickets/edit_ticket.php?id=<?= $row["id"]; ?>"><?= $row["id"] ?></a></td>
-                <td data-cell="Subject"><a href="/controllers/tickets/edit_ticket.php?id=<?= $row["id"]; ?>"><?= $row["name"] ?></a></td>
-                <td data-cell="Request Detail"><?= limitChars(html_entity_decode($row["description"]), 100) ?></td>
+                <td data-cell="ID"><a href="/controllers/tickets/edit_ticket.php?id=<?= $ticket_row["id"]; ?>"><?= $ticket_row["id"] ?></a></td>
+                <td data-cell="Subject"><a href="/controllers/tickets/edit_ticket.php?id=<?= $ticket_row["id"]; ?>"><?= $ticket_row["name"] ?></a></td>
+                <td data-cell="Request Detail"><?= limitChars(html_entity_decode($ticket_row["description"]), 100) ?></td>
                 <td data-cell="Location">
                     <?php
                     // Query the sites table to get the location name
-                    $location_query = "SELECT location_name FROM locations WHERE sitenumber = " . $row["location"];
+                    $location_query = "SELECT location_name FROM locations WHERE sitenumber = " . $ticket_row["location"];
                     $location_result = mysqli_query($database, $location_query);
                     $location_name = mysqli_fetch_assoc($location_result)['location_name'];
 
                     // Display the location name and room number
-                    echo $location_name . '<br><br>RM ' . $row['room'];
+                    echo $location_name . '<br><br>RM ' . $ticket_row['room'];
                     ?>
                 </td>
                 <td data-cell="Category">
                     <?php
-                    if ($row['request_type_id'] === '0') {
+                    if ($ticket_row['request_type_id'] === '0') {
                         echo "Other";
                     } else {
-                        $request_type_query = "SELECT request_name FROM request_type WHERE request_id = " . $row['request_type_id'];
+                        $request_type_query = "SELECT request_name FROM request_type WHERE request_id = " . $ticket_row['request_type_id'];
                         $request_type_query_result = mysqli_query($database, $request_type_query);
                         $request_type_name = mysqli_fetch_assoc($request_type_query_result)['request_name'];
                         echo $request_type_name;
                     }
                     ?>
                 </td>
-                <td data-cell="Assigned Employee"><?= $row['employee'] ?></td>
-                <td data-cell="Current Status"><?= $row['status'] ?></td>
+                <td data-cell="Assigned Employee"><?= $ticket_row['employee'] ?></td>
+                <td data-cell="Current Status"><?= $ticket_row['status'] ?></td>
                 <td data-cell="Created"><?= $created ?></td>
                 <td data-cell="Last Updated"><?= $last_update ?></td>
                 <?php if ($overdue) { ?>

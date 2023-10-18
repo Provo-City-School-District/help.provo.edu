cb11f24a2d70824cb88efb16898bc6827f60cbd8
Josh Espinoza <joshespi@gmail.com>
2023-10-18 06:37:08 -0600

    adding name call to profile page

--- src/profile.php
+++ src/profile.php
@@ -1,7 +1,17 @@
 <?php
 include("includes/header.php");
-?>
+include("includes/helpdbconnect.php");
 
+$user_query = "SELECT * FROM users WHERE username = '" . $_SESSION['username'] . "'";
+$user_result = mysqli_query($database, $user_query);
+// Check if the query was successful
+if (!$user_result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+$user_data = mysqli_fetch_assoc($user_result);
+print_r($user_data);
+?>
+<h1>Profile For <?= $user_data['firstname'] . ' ' . $user_data['lastname'] ?> (<span><?= $user_data['username'] ?></span>)</h1>
 Potential features
 -profile modifications
 -see ticket stats?

071a4864fb45e34db672b1c622494984bc830536
Braxton Anderson <braxtona@provo.edu>
2023-11-06 08:11:19 -0700

    more incoming email code work

--- src/incoming_email.php
+++ src/incoming_email.php
@@ -1,31 +1,11 @@
 <?php
 include("includes/header.php");
+// TESTING CODE + FILE
 
-// handles input validation
-function validate_incoming_email(
-    string $sender,
-    string $subject,
-    string $message)
-{
-    $email_note = [];
-    
-    // subject "ticket 30" -> 30
-    $subject_split = explode(' ', $subject);
-    $ticket_value = intval($subject_split[1]);
-    if ((strtolower($subject_split[0]) == "ticket") && (count($subject_split) == 2) && ($ticket_value > 0))
-        $email_note["ticket"] = $ticket_value;
-    else
-        $email_note["ticket"] = null;
-
-    $email_note["content"] = trim(htmlspecialchars($message));
-    $email_note["sender"] = trim(htmlspecialchars($sender));
-    
-    return $email_note;
-}
 
 $imap_path = '{imap.gmail.com:993/imap/ssl}INBOX';
-$username = 'dev@provo.edu';
-$password = 'fatigue7endogamy!hacienda1thin6patio9upward8honeydew8FIGHT8avarice0culprit';
+$username = getenv("GMAIL_USER");
+$password = getenv("GMAIL_PASSWORD");
 
 $mbox = imap_open($imap_path, $username, $password, OP_READONLY) or die('Cannot connect to Gmail: ' . imap_last_error());
 
@@ -37,10 +17,20 @@ $msg_count = $mboxCheck->Nmsgs;
 // iterate trough those messages
 for ($i = 1; $i <= $msg_count; $i++) {
     $header = imap_headerinfo($mbox, $i);
+    $from_host = $header->from[0]->host;
+    
+    $sender = $header->from[0]->mailbox . "@" . $from_host;
+    $subject = $header->subject;
+    echo $header->recent;
+    $content = imap_body($mbox, $i);
 
-    $from_addr = $header->from[0]->mailbox . "@" . $header->from[0]->host;
+    // Ignore non district emails
+    //if ((strtolower($from_host) != "provo.edu"))
+    //    continue;
 
-    parse_incoming_email($from_addr, $subject, $message);
+    echo preg_replace( "/\n\s+/", "\n", rtrim(html_entity_decode(strip_tags($content))));
+    echo "<br><br>";
+
 }
 
 imap_close($mbox);

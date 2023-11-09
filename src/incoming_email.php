a0c941a79755448aa08f3e1377f4308bd5125ff3
Braxton Anderson <braxtona@provo.edu>
2023-11-09 10:38:16 -0700

    worked on email code - successfully reads inbox and moves all messages to important folder after they have been read

--- src/incoming_email.php
+++ src/incoming_email.php
@@ -7,32 +7,38 @@ $imap_path = '{imap.gmail.com:993/imap/ssl}INBOX';
 $username = getenv("GMAIL_USER");
 $password = getenv("GMAIL_PASSWORD");
 
-$mbox = imap_open($imap_path, $username, $password, OP_READONLY) or die('Cannot connect to Gmail: ' . imap_last_error());
+$mbox = imap_open($imap_path, $username, $password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-// get information about the current mailbox (INBOX in this case)
-$mboxCheck = imap_check($mbox);
+$msg_count = imap_num_msg($mbox);
+echo "Msg count: ".$msg_count."<br><br>";
 
-$msg_count = $mboxCheck->Nmsgs;
-
-// iterate trough those messages
+// iterate through those messages
 for ($i = 1; $i <= $msg_count; $i++) {
     $header = imap_headerinfo($mbox, $i);
     $from_host = $header->from[0]->host;
     
     $sender = $header->from[0]->mailbox . "@" . $from_host;
     $subject = $header->subject;
-    echo $header->recent;
+
     $content = imap_body($mbox, $i);
 
     // Ignore non district emails
-    //if ((strtolower($from_host) != "provo.edu"))
-    //    continue;
+    if ((strtolower($from_host) != "provo.edu"))
+        continue;
 
-    echo htmlspecialchars($content);
-    echo "<br><br>";
+    // Parse ticket here
+}
 
+// Move parsed emails to important folder/label
+if ($msg_count > 0) {
+    $msg_move_result = imap_mail_move($mbox, "1:".strval($msg_count), "[Gmail]/Important");
+    if (!$msg_move_result) {
+        die("Failed to move message: ".imap_last_error());
+    }
 }
 
 imap_close($mbox);
-
-echo "==end==";
+?>
+<br><br><br>
+----------------------<br>
+End of messages

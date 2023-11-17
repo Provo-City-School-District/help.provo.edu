e1e5af51fe4d3cbde03bfc4ab7fda6d096c99a82
Josh Espinoza <joshespi@gmail.com>
2023-11-17 07:06:06 -0700

    session regeneration to mitigate risk of session fixation

--- src/includes/init.php
+++ src/includes/init.php
@@ -2,4 +2,6 @@
 // Checks if Session exists, if not starts one.
 if (!session_id()) {
     session_start();
+    // Regenerate the session ID to mitigate the risk of session fixation.
+    session_regenerate_id(true);
 }

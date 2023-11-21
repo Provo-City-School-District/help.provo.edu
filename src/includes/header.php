700a1a3379136fa07c12a8ffd0987c64560ee9df
Braxton Anderson <braxtona@provo.edu>
2023-11-21 09:13:52 -0700

    converted some more mysqli_query to prepared statements

--- src/includes/header.php
+++ src/includes/header.php
@@ -134,7 +134,7 @@ $admin_page = '/admin.php';
                             SELECT 1
                             FROM tickets
                             WHERE status NOT IN ('Closed', 'Resolved')
-                            AND employee = '$username'
+                            AND employee = ?
                             ORDER BY id ASC
                         STR;
 
@@ -143,18 +143,30 @@ $admin_page = '/admin.php';
                         WHERE
                             tickets.id in (
                                 SELECT flagged_tickets.ticket_id from flagged_tickets WHERE flagged_tickets.user_id in (
-                                    SELECT users.id FROM users WHERE users.username = '$username'
+                                    SELECT users.id FROM users WHERE users.username = ?
                                 )
                             )
                         STR;
-        
-                        $num_assigned_tickets_result = mysqli_query($database, $num_assigned_tickets_query);
-                        if ($num_assigned_tickets_result)
-                            $num_assigned_tickets = mysqli_num_rows($num_assigned_tickets_result);
 
-                        $num_flagged_tickets_result = mysqli_query($database, $num_flagged_tickets_query);
-                        if ($num_flagged_tickets_result)
-                            $num_flagged_tickets = mysqli_num_rows($num_flagged_tickets_result);
+                        $assigned_stmt = mysqli_prepare($database, $num_assigned_tickets_query);
+                        mysqli_stmt_bind_param($assigned_stmt, "s", $username);
+                        $assigned_stmt_succeeded = mysqli_stmt_execute($assigned_stmt);
+                        $assigned_res = mysqli_stmt_get_result($assigned_stmt);
+
+                        if ($assigned_stmt_succeeded)
+                            $num_assigned_tickets = mysqli_num_rows($assigned_res);
+
+
+                        $flagged_stmt = mysqli_prepare($database, $num_flagged_tickets_query);
+                        mysqli_stmt_bind_param($flagged_stmt, "s", $username);
+                        $flagged_stmt_succeeded = mysqli_stmt_execute($flagged_stmt);
+                        $flagged_res = mysqli_stmt_get_result($flagged_stmt);
+
+                        if ($flagged_stmt_succeeded)
+                            $num_flagged_tickets = mysqli_num_rows($flagged_res);
+    
+                        mysqli_stmt_close($assigned_stmt);
+                        mysqli_stmt_close($flagged_stmt);
                     ?>
                     
                         <li><a href="/tickets.php">Assigned Tickets (<?= $num_assigned_tickets ?>)</a></li>

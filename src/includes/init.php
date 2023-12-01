eeb8d54e872224e063ce17350a6529031515ea7b
Josh Espinoza <joshespi@gmail.com>
2023-12-01 08:27:23 -0700

    removed session regen from this file. I read it was better to just do this on success login

--- src/includes/init.php
+++ src/includes/init.php
@@ -2,6 +2,4 @@
 // Checks if Session exists, if not starts one.
 if (!session_id()) {
     session_start();
-    // Regenerate the session ID to mitigate the risk of session fixation.
-    session_regenerate_id(true);
 }

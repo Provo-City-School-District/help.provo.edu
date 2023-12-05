6ffa1816d29267dfddeaefad22a15b2b38410a79
Josh Espinoza <joshespi@gmail.com>
2023-12-05 11:39:46 -0700

    added alert_level column fill

--- src/includes/ticket_alerts.php
+++ src/includes/ticket_alerts.php
@@ -2,7 +2,7 @@
 require_once('helpdbconnect.php');
 
 // insert an alert into the alerts table if it doesn't already exist
-function insertAlertIfNotExists($database, $ticket, $alertMessage)
+function insertAlertIfNotExists($database, $ticket, $alertMessage, $alertLevel)
 {
     // Prepare a SQL statement to select alerts with the same ticket_id, employee, and message
     $selectAlertsQuery = "SELECT 1 FROM alerts WHERE ticket_id = ? AND employee = ? AND message = ?";
@@ -17,9 +17,9 @@ function insertAlertIfNotExists($database, $ticket, $alertMessage)
     if (!$selectAlertsStmt->fetch()) {
         $selectAlertsStmt->close();
 
-        $insertAlertQuery = "INSERT INTO alerts (ticket_id, employee, message) VALUES (?, ?, ?)";
+        $insertAlertQuery = "INSERT INTO alerts (ticket_id, employee, message, alert_level) VALUES (?, ?, ?, ?)";
         $insertAlertStmt = $database->prepare($insertAlertQuery);
-        $insertAlertStmt->bind_param("iss", $ticket['id'], $ticket['employee'], $alertMessage);
+        $insertAlertStmt->bind_param("isss", $ticket['id'], $ticket['employee'], $alertMessage, $alertLevel);
         $insertAlertStmt->execute();
         $insertAlertStmt->close();
     } else {
@@ -64,6 +64,10 @@ while ($selectTicketsStmt->fetch()) {
 
 $selectTicketsStmt->close();
 
+// Current Alerts available
+// warn - Ticket hasn't been updated in 48 hours
+// crit - Past Due
+
 // For each ticket, insert an alert into the alerts table
 foreach ($oldTickets as $oldTicket) {
     // Convert the last_updated time to a DateTime object
@@ -71,14 +75,14 @@ foreach ($oldTickets as $oldTicket) {
 
     // If the last_updated time is longer than two days ago, insert an alert for 48 hours since last update
     if ($lastUpdated < $twoDaysAgo) {
-        insertAlertIfNotExists($database, $oldTicket, $alert48Message);
+        insertAlertIfNotExists($database, $oldTicket, $alert48Message, 'warn');
     }
 
     // Alert for past due
     $daysUntilDueDate = getDaysUntilDueDate($oldTicket['due_date']);
     // If the ticket's priority is greater than the number of days until its due date, insert a new alert
     if ($oldTicket['priority'] > $daysUntilDueDate) {
-        insertAlertIfNotExists($database, $oldTicket, $pastDueMessage);
+        insertAlertIfNotExists($database, $oldTicket, $pastDueMessage, 'crit');
     }
 }
 

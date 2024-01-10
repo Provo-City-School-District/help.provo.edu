32f3a0066fabcf1e3c821297343149f970d5488a
Josh Espinoza <joshespi@gmail.com>
2024-01-10 13:18:06 -0700

    modified to display alerts for subordinate users

--- src/controllers/tickets/subordinate_tickets.php
+++ src/controllers/tickets/subordinate_tickets.php
@@ -1,6 +1,4 @@
 <?php
-require_once("block_file.php");
-require_once(from_root("/includes/tickets_template.php"));
 include("header.php");
 
 if ($_SESSION['permissions']['is_admin'] != 1) {
@@ -12,23 +10,40 @@ if ($_SESSION['permissions']['is_admin'] != 1) {
     }
 }
 require_once('helpdbconnect.php');
-include("ticket_utils.php");
+require_once(from_root("/includes/block_file.php"));
+require_once(from_root("/includes/ticket_utils.php"));
+require_once(from_root("/includes/tickets_template.php"));
+require_once(from_root("/includes/alerts_template.php"));
 
-//page query
-$ticket_query = "
-SELECT tickets.* 
-FROM tickets 
-JOIN users ON tickets.employee = users.username 
-WHERE users.supervisor_username = '" . $_SESSION['username'] . "' 
-AND tickets.status NOT IN ('closed', 'resolved') 
-ORDER BY tickets.last_updated DESC";
+$username = $_SESSION['username'];
 
-$ticket_result = mysqli_query($database, $ticket_query);
+if ($_SESSION['permissions']['is_supervisor'] == 1) {
+    //Tickets query
+    $ticket_query = <<<STR
+    SELECT tickets.* 
+    FROM tickets 
+    JOIN users ON tickets.employee = users.username 
+    WHERE users.supervisor_username = '$username' 
+    AND tickets.status NOT IN ('closed', 'resolved') 
+    ORDER BY tickets.last_updated DESC
+    STR;
+
+    $ticket_result = mysqli_query($database, $ticket_query);
+
+    //Alerts query
+    $alerts_query = <<<STR
+    SELECT alerts.* 
+    FROM alerts 
+    JOIN users ON alerts.employee = users.username
+    WHERE users.supervisor_username = '$username' 
+    STR;
+
+    $alerts_result = mysqli_query($database, $alerts_query);
+
+    echo ' <h1>Subordinate Tickets</h1>';
+    display_ticket_alerts($alerts_result);
+    display_tickets_table($ticket_result, $database);
+}
+include("footer.php");
 
 ?>
-
-<h1>Subordinate Tickets</h1>
-
-<?php display_tickets_table($ticket_result, $database); ?>
-
-<?php include("footer.php"); ?>

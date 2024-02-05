7a5c543d5f62c1c4d0de266d22cebef2aebfefcf
Josh Espinoza <joshespi@gmail.com>
2024-02-05 11:42:47 -0700

    Add status popup and session management to index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
-<?php include("header.php"); ?>
+require_once("status_popup.php");
+session_start();
+session_regenerate_id(true);
+// Display Status Popup
+if (isset($_SESSION['current_status'])) {
+    $status_popup = new StatusPopup($_SESSION["current_status"], StatusPopupType::fromString($_SESSION["status_type"]));
+    echo $status_popup;
+
+    unset($_SESSION['current_status']);
+    unset($_SESSION['status_type']);
+}
 <div id="loginWrapper">
     <h1>Login for Help</h1>
 

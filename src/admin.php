9691ea7fb792f38a84797058e40e32d00103dd78
Josh Espinoza <joshespi@gmail.com>
2024-02-05 14:56:36 -0700

    commented out sections that appear on the supervisor page

--- src/admin.php
+++ src/admin.php
@@ -9,33 +9,33 @@ if ($_SESSION['permissions']['is_admin'] != 1) {
 }
 
 require_once('helpdbconnect.php');
-require("ticket_utils.php");
+// require("ticket_utils.php");
 
 // process the data for admin report charts
-function process_query_result($query_result, $label_field)
-{
-    $count = [];
+// function process_query_result($query_result, $label_field)
+// {
+//     $count = [];
 
-    while ($row = mysqli_fetch_assoc($query_result)) {
-        $label = $row[$label_field];
-        if ($label == null || $label == "")
-            $label = "unassigned";
+//     while ($row = mysqli_fetch_assoc($query_result)) {
+//         $label = $row[$label_field];
+//         if ($label == null || $label == "")
+//             $label = "unassigned";
 
-        if (!isset($count[$label]))
-            $count[$label] = 1;
-        else
-            $count[$label]++;
-    }
+//         if (!isset($count[$label]))
+//             $count[$label] = 1;
+//         else
+//             $count[$label]++;
+//     }
 
-    asort($count);
+//     asort($count);
 
-    $processedData = [];
-    foreach ($count as $name => $count) {
-        $processedData[] = array("y" => $count, "label" => $name);
-    }
+//     $processedData = [];
+//     foreach ($count as $name => $count) {
+//         $processedData[] = array("y" => $count, "label" => $name);
+//     }
 
-    return $processedData;
-}
+//     return $processedData;
+// }
 
 // Execute the SELECT query to retrieve all users from the users table
 $users_query = "SELECT * FROM users ORDER BY username ASC";
@@ -56,42 +56,42 @@ if (isset($_SESSION['current_status'])) {
 
 
 // Query open tickets based on tech
-$tech_query = <<<STR
-    SELECT employee FROM tickets WHERE status NOT IN ('closed', 'resolved')
-    STR;
-$tech_query_result = mysqli_query($database, $tech_query);
-$allTechs = process_query_result($tech_query_result, "employee");
+// $tech_query = <<<STR
+//     SELECT employee FROM tickets WHERE status NOT IN ('closed', 'resolved')
+//     STR;
+// $tech_query_result = mysqli_query($database, $tech_query);
+// $allTechs = process_query_result($tech_query_result, "employee");
 
 // Query open tickets based on location:
-$location_query = <<<STR
-    SELECT locations.location_name, tickets.location
-    FROM tickets 
-    INNER JOIN locations ON tickets.location = locations.sitenumber 
-    WHERE tickets.status NOT IN ('closed', 'resolved')
-STR;
+// $location_query = <<<STR
+//     SELECT locations.location_name, tickets.location
+//     FROM tickets 
+//     INNER JOIN locations ON tickets.location = locations.sitenumber 
+//     WHERE tickets.status NOT IN ('closed', 'resolved')
+// STR;
 
-$location_query_result = mysqli_query($database, $location_query);
-$allLocations = process_query_result($location_query_result, "location_name");
+// $location_query_result = mysqli_query($database, $location_query);
+// $allLocations = process_query_result($location_query_result, "location_name");
 
 // Query open tickets based on field tech:
-$field_tech_query = <<<STR
-    SELECT tickets.employee 
-    FROM tickets 
-    INNER JOIN users ON tickets.employee = users.username 
-    WHERE tickets.status NOT IN ('closed', 'resolved') AND users.is_tech = 1
-STR;
+// $field_tech_query = <<<STR
+//     SELECT tickets.employee 
+//     FROM tickets 
+//     INNER JOIN users ON tickets.employee = users.username 
+//     WHERE tickets.status NOT IN ('closed', 'resolved') AND users.is_tech = 1
+// STR;
 
-$field_tech_query_result = mysqli_query($database, $field_tech_query);
-$fieldTechs = process_query_result($field_tech_query_result, "employee");
+// $field_tech_query_result = mysqli_query($database, $field_tech_query);
+// $fieldTechs = process_query_result($field_tech_query_result, "employee");
 
 ?>
 <h1>Admin</h1>
-<h2>Reports</h2>
+<!-- <h2>Reports</h2>
 <div class="grid3 canvasjsreport">
     <div id="techOpenTicket" style="height: 370px; width: 100%;"></div>
     <div id="byLocation" style="height: 370px; width: 100%;"></div>
     <div id="fieldTechOpen" style="height: 370px; width: 100%;"></div>
-</div>
+</div> -->
 
 <h2>All Users</h2>
 <table class="allUsers data-table">
@@ -171,7 +171,7 @@ $exclude_result = mysqli_query($database, $exclude_query);
     Source Ticket ID:<input type="text" id="ticket_id_source" name="ticket_id_source" value=""><br>
     <button type="submit">Merge</button><br>
 </form>
-<h2>Unassigned Tickets</h2>
+<h2>All Unassigned Tickets</h2>
 
 <?php
 //query for unassigned tickets
@@ -184,9 +184,9 @@ $ticket_result = mysqli_query($database, $ticket_query);
 display_tickets_table($ticket_result, $database);
 ?>
 
-<script>
+<!-- <script>
     let allTechs = <?php echo json_encode($allTechs, JSON_NUMERIC_CHECK); ?>;
     let byLocation = <?php echo json_encode($allLocations, JSON_NUMERIC_CHECK); ?>;
     let fieldTechOpen = <?php echo json_encode($fieldTechs, JSON_NUMERIC_CHECK); ?>;
-</script>
+</script> -->
 <?php include("footer.php"); ?>

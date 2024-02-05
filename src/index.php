71008772d8b07533734e63f048cb7b31103716b1
Josh Espinoza <joshespi@gmail.com>
2024-02-05 11:42:57 -0700

    Add login check and redirect to tickets page if user is already logged in

--- src/index.php
+++ src/index.php
@@ -1,6 +1,18 @@
+<?php
 require_once("status_popup.php");
 session_start();
 session_regenerate_id(true);
+
+// Check if user is already logged in
+if (isset($_SESSION['username'])) {
+    header('Location: tickets.php');
+    exit();
+}
+?>
+
+<?php 
+// Display Front End
+include("header.php"); 
 // Display Status Popup
 if (isset($_SESSION['current_status'])) {
     $status_popup = new StatusPopup($_SESSION["current_status"], StatusPopupType::fromString($_SESSION["status_type"]));
@@ -9,6 +21,8 @@ if (isset($_SESSION['current_status'])) {
     unset($_SESSION['current_status']);
     unset($_SESSION['status_type']);
 }
+
+?>
 <div id="loginWrapper">
     <h1>Login for Help</h1>
 

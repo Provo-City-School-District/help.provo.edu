81273826264212f9557a9baeac359cdaeda9fbff
Josh Espinoza <joshespi@gmail.com>
2024-02-07 12:57:31 -0700

    set color preference for JS options

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -7,6 +7,9 @@
 <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 <script src="/includes/js/dataTables-1.13.7/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="/vendor/tinymce/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    var userPref = '<?php echo isset($_SESSION['color_scheme']) ? $_SESSION['color_scheme'] : 'light'; ?>';
+</script>
 <script src="/includes/js/main.js?v=1.01.01" type="text/javascript"></script>
 </body>
 

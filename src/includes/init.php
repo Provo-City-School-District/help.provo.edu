099d2abc45461750e96b2815eb6c604689f3f583
Josh Espinoza <joshespi@gmail.com>
2024-02-11 12:33:38 -0700

    moved session lifetime to ini as in the init file seemed to not be behaving

--- src/includes/init.php
+++ src/includes/init.php
@@ -1,7 +1,6 @@
 <?php
 // Checks if Session exists, if not starts one.
 if (!session_id()) {
-    session_set_cookie_params(7200); // 2 hours until auto logout
     session_start();
     session_regenerate_id(true);
 }

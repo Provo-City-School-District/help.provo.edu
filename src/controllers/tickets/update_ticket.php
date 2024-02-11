5259e6f165a0658cc0e8e218cbfa0c29bec681c8
Josh Espinoza <joshespi@gmail.com>
2024-02-10 18:56:26 -0700

    some idiot removed these >_>

--- src/controllers/tickets/update_ticket.php
+++ src/controllers/tickets/update_ticket.php
@@ -236,7 +236,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $template = new Template(from_root("/includes/templates/ticket_updated.phtml"));
         $template->ticket_id = $ticket_id;
-        // $template->changes_message = $changesMessage;
+        $template->changes_message = $changesMessage;
         $template->notes_message = $notesMessage;
         $template->site_url = getenv('ROOTDOMAIN');
         $template->description = html_entity_decode($updatedDescription);
@@ -271,7 +271,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $template = new Template(from_root("/includes/templates/ticket_resolved.phtml"));
         $template->ticket_id = $ticket_id;
-        // $template->changes_message = html_entity_decode($changesMessage);
+        $template->changes_message = html_entity_decode($changesMessage);
         $template->notes_message = $notesMessage;
         $template->site_url = getenv('ROOTDOMAIN');
         $template->description = html_entity_decode($updatedDescription);

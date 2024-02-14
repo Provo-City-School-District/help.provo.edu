70dc6fcc98c544f6eddc25a1666a0b02da897ad6
Josh Espinoza <joshespi@gmail.com>
2024-02-14 07:00:51 -0700

    prevent double form submissions

--- src/includes/js/main.js
+++ src/includes/js/main.js
@@ -204,4 +204,16 @@ if (updateTicketForm) {
     }
   });
 }
+//================================= Prevent Form Double Submit =================================
+// Prevent Double Submits
+document.querySelectorAll("form").forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    // Prevent if already submitting
+    if (form.classList.contains("is-submitting")) {
+      e.preventDefault();
+    }
+
+    // Add class to hook our visual indicator on
+    form.classList.add("is-submitting");
+  });
   });

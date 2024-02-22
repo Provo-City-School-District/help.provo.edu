e2a76407409343ae88fbfbd43735d77a356172d2
Braxton Anderson <braxtona@provo.edu>
2024-02-22 14:03:17 -0700

    fix buggy session cookie

--- src/includes/init.php
+++ src/includes/init.php
@@ -3,7 +3,3 @@
 if (!session_id()) {
     session_start();
 }
-
-// Reset cookie lifetime each time this is loaded. Included by header.php
-$lifetime = 7200;
-setcookie(session_name(), session_id(), time() + $lifetime);

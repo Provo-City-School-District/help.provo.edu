154ec343183bba92f425ca401cc6b85e621c3802
Josh Espinoza <joshespi@gmail.com>
2024-02-26 13:39:06 -0700

    check if logged in longer than 2 hours ago and logs them out which is pre-mature to the server limits

--- src/includes/init.php
+++ src/includes/init.php
@@ -11,7 +11,26 @@ $inactivity_time = 60 * 60; // 60 minutes
 
 // Check if the last_timestamp session variable is set
 if (isset($_SESSION['last_timestamp'])) {
+    $user_last_login = get_last_login_time($_SESSION['username']);
     $current_time = time();
+
+    // Calculate the time difference in seconds
+    $time_difference = $current_time - $user_last_login;
+
+    if ($time_since_last_login > 2 * 60 * 60) {
+        // Unset all session variables
+        $_SESSION = array();
+        log_app(LOG_INFO, $_SESSION['username'] . " Session killed for being older than 2 hours");
+        // Delete the session cookie
+        session_unset();
+        // Destroy the session
+        session_destroy();
+
+        // Redirect to the login page
+        header('Location: /index.php');
+        exit;
+    }
+
     // Calculate the inactivity time
     $inactivity = $current_time - $_SESSION['last_timestamp'];
 

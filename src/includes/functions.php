d8a4b720d43c0c5f2d9ff2e97a17205a9130e2ea
Josh Espinoza <joshespi@gmail.com>
2024-02-26 13:38:02 -0700

    moved functions back to proper location

--- src/includes/functions.php
+++ src/includes/functions.php
@@ -177,3 +177,34 @@ function isUserATech($usernameToCheck, $databaseConnection)
 
     return $userData['is_tech'] == 1;
 }
+
+function log_app(int $priority, string $message)
+{
+    openlog("appLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+    syslog($priority, $message);
+    closelog();
+}
+function get_last_login_time($username)
+{
+    global $database;
+    // Prepare a SQL statement
+    $query_stmt = $database->prepare('SELECT last_login FROM users WHERE username = ?');
+
+    // Bind the username parameter
+    $query_stmt->bind_param('s', $username);
+
+    // Execute the statement
+    $query_stmt->execute();
+
+    // Bind the result to a variable
+    $query_stmt->bind_result($last_login);
+
+    // Fetch the result
+    $query_stmt->fetch();
+
+    // Close the statement
+    $query_stmt->close();
+
+    // Return the last login time
+    return $last_login;
+}
--- src/includes/init.php
+++ src/includes/init.php
@@ -1,16 +1,10 @@
 <?php
+include_once('functions.php');
+include_once('helpdbconnect.php');
 // Checks if Session exists, if not starts one.
 if (!session_id()) {
     session_start();
 }
-
-function log_app(int $priority, string $message)
-{
-    openlog("appLog", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-    syslog($priority, $message);
-    closelog();
-}
-
 
 // Set the inactivity time
 $inactivity_time = 60 * 60; // 60 minutes

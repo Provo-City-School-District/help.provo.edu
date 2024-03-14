6fe79940379aa2137498ebc6971c96f45502a471
Josh Espinoza <joshespi@gmail.com>
2024-03-14 07:24:46 -0600

    rework time input behavior. WO127112

--- src/controllers/tickets/edit_ticket.php
+++ src/controllers/tickets/edit_ticket.php
@@ -958,38 +958,6 @@ if (isset($ticket["client"])) {
 </script>
 <?php if ($_SESSION['permissions']['is_tech']) : ?>
     <script>
-        //calc time on the fly
-        document.querySelectorAll('#work_hours, #work_minutes, #travel_hours, #travel_minutes').forEach(function(el) {
-            el.addEventListener('input', function() {
-                const workHours = parseInt(document.getElementById('work_hours').value) || 0;
-                const workMinutes = parseInt(document.getElementById('work_minutes').value) || 0;
-                const travelHours = parseInt(document.getElementById('travel_hours').value) || 0;
-                const travelMinutes = parseInt(document.getElementById('travel_minutes').value) || 0;
-
-                const totalTime = (workHours + travelHours) * 60 + workMinutes + travelMinutes;
-
-                document.getElementById('total_time').value = totalTime;
-            });
-        });
-
-        // add alert if no time is entered
-        $("#note-submit").on("submit", function(evt) {
-            let fields = ['work_hours', 'work_minutes', 'travel_hours', 'travel_minutes'];
-            let allZero = fields.every(function(field) {
-                return parseInt(document.getElementById(field).value, 10) === 0;
-            });
-
-            const note_content = tinymce.activeEditor.getContent("note");
-            console.log(note_content);
-            if (allZero) {
-                alert('Please enter a value greater than 0 for at least one of the time fields.');
-                evt.preventDefault(); // Prevent the form submission
-            } else if (!note_content) {
-                alert('Please enter some note content');
-                evt.preventDefault();
-            }
-        });
-
         function updateTimeSinceLastNote() {
             // Loop over each element with the class 'created_date'
             $('.created_date').each(function() {
@@ -1030,6 +998,7 @@ if (isset($ticket["client"])) {
     </script>
 <?php endif; ?>
 <?php include("footer.php"); ?>
+<script src="/includes/js/note_time.js" type="text/javascript"></script>
 <script>
     function split(val) {
         return val.split(/,\s*/);

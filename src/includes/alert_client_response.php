47f626ba9bc8289ee24ba6790ace7536269a7bae
Josh Espinoza <joshespi@gmail.com>
2024-03-19 09:50:27 -0600

    get and process notes like on update_ticket.php

--- src/includes/alert_client_response.php
+++ src/includes/alert_client_response.php
@@ -11,8 +11,37 @@ try {
     $client_response_tickets = $client_response_tickets_results->fetch_all(MYSQLI_ASSOC);
 
     foreach ($client_response_tickets as $ticket) {
-        // process each ticket
-        echo $ticket['id'] . ", " . $ticket['name'] . "\n";
+        // Get Notes  - reused from update_ticket.php
+        $notes = get_ticket_notes($ticket['id'], 3);
+        $notesMessageClient = "";
+        if (count($notes) > 0) {
+            $notesMessageClient .= "<tr><th>Date</th><th>Creator</th><th>Note</th></tr>";
+        // Build Notes for Email - reused from update_ticket.php
+        foreach ($notes as $note) {
+            $dateOverride = $note['date_override'];
+            $effectiveDate = $dateOverride;
+            if ($dateOverride == null)
+                $effectiveDate = $note['created'];
+
+            $dateStr = date_format(date_create($effectiveDate), "F jS\, Y h:i:s A");
+            $noteCreator = $note['creator'];
+            $decodedNote = htmlspecialchars_decode($note['note']);
+
+            $note_theme = "";
+            if (!user_is_tech($noteCreator)) {
+                $note_theme = "nonTech";
+            } else if ($note['visible_to_client'] == 0) {
+                $note_theme = "notClientVisible";
+            } else {
+                $note_theme = "clientVisible";
+            }
+
+            $notesMessageTech .= "<tr><td>$dateStr</td><td>$noteCreator</td><td><span class=\"$note_theme\">$decodedNote</span></td></tr>";
+            if ($note['visible_to_client']) {
+                $notesMessageClient .= "<tr><td>$dateStr</td><td>$noteCreator</td><td><span class=\"$note_theme\">$decodedNote</span></td></tr>";
+            }
+        }
+
     }
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";

472c59d682b315b683280afca28335ea21c4c98a
Josh Espinoza <joshespi@gmail.com>
2024-03-19 11:20:31 -0600

    removed unused code/comments

--- src/includes/alert_client_response.php
+++ src/includes/alert_client_response.php
@@ -4,9 +4,6 @@ require_once('ticket_utils.php');
 require_once("email_utils.php");
 require("template.php");
 
-// Todays date
-$today = new DateTime();
-$template_path = "ticket_response_required";
 try {
     $client_response_tickets_query = "SELECT * FROM tickets WHERE priority = 15";
     $client_response_tickets_results = $database->execute_query($client_response_tickets_query);
@@ -54,34 +51,8 @@ try {
             }
         }
 
-
-
-
-        // Array (
-        //         [id] => 127425
-        //         [created] => 2024-03-15 06:52:03
-        //         [due_date] => 2024-04-10
-        //         [last_updated] => 2024-03-15 13:51:48
-        //         [name] => Door Sensor Monitors
-        //         [description] => &lt;p&gt;We are going to switch email reports to weekly instead of daily&lt;/p&gt;
-        //         [location] => 1896
-        //         [room] => 
-        //         [employee] => joshe
-        //         [client] => CharityW
-        //         [status] => open
-        //         [attachment_path] => 
-        //         [phone] => 
-        //         [request_type_id] => 143
-        //         [cc_emails] => chadd@provo.edu
-        //         [bcc_emails] => 
-        //         [priority] => 15
-        //         [merged_into_id] => 
-        //         [parent_ticket] => 0
-        //         [email_msg_id] => 
-        //     )
-
-
         // Build Email Template
+        $template_path = "ticket_response_required";
         $template_client = new Template(__DIR__ . "/templates/{$template_path}_client.phtml");
 
         $client_name = get_local_name_for_user($ticket['client']);
@@ -92,15 +63,19 @@ try {
         $template_client->site_url = getenv('ROOTDOMAIN');
         $template_client->description = html_entity_decode($ticket['description']);
 
-        // print_r($template_client);
+        // Set Email Variables
         $ticket['client'] = 'joshe@provo.edu';
         // omitting the email CC,BCC,Attachments on client reminder
         $ticket['cc_emails'] = array();
         $ticket['bcc_emails'] = array();
         $ticket['attachment_path'] = array();
+
+        $ticket_subject = "Response Required For: " . $ticket['id'] . " - " . $ticket['name'];
+
+        // Send Email
+        // can be used in debugging log mode later
         // log_app(LOG_INFO, "Sending email to Tickets with priority 15. Ticket ID: " . $ticket['id'] . " Title: " . $ticket['name']);
-        send_email_and_add_to_ticket($ticket['id'], $ticket['client'], $ticket['name'], $template_client, $ticket['cc_emails'], $ticket['bcc_emails'], $ticket['attachment_path']);
-        // echo $ticket['id'] . ", " . $ticket['name'] . "\n";
+        send_email_and_add_to_ticket($ticket['id'], $ticket['client'], $ticket_subject, $template_client, $ticket['cc_emails'], $ticket['bcc_emails'], $ticket['attachment_path']);
     }
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";

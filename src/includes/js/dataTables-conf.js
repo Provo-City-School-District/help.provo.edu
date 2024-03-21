1c6a0ab9ad1ba8bd88d757ebdba044095405d6d1
Josh Espinoza <joshespi@gmail.com>
2024-03-21 14:00:17 -0600

    refactored up config for data-tables

--- src/includes/js/dataTables-conf.js
+++ src/includes/js/dataTables-conf.js
@@ -3,56 +3,49 @@ var table;
 
 // Initialize the data table library on the table with the class data-table
 $(document).ready(function () {
+  // Define general properties
+  var options = {
+    paging: true, // Enable pagination
+    stateSave: true, // Enable state saving
+    ordering: true, // Enable sorting
+    columns: [
+      { width: "5%" },
+      { width: "20%" },
+      { width: "10%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+      { width: "5%" },
+    ],
+  };
+
+  // Modify properties for specific cases
   if (
     window.location.pathname === "/controllers/tickets/ticket_history.php" ||
     window.location.pathname === "/controllers/tickets/recent_tickets.php" ||
     window.location.pathname === "/controllers/tickets/flagged_tickets.php"
   ) {
-    table = $(".data-table").DataTable({
-      order: [[9, "desc"]], // Sort by the second column in ascending order
-      stateSave: true, // Enable state saving
-    });
+    options.order = [[9, "desc"]];
   } else if (
     window.location.pathname === "/supervisor.php" ||
     window.location.pathname === "/admin.php" ||
     window.location.pathname === "/tickets.php"
   ) {
-    table = $(".data-table").DataTable({
-      order: [[8, "asc"]], // Sort by the second column in ascending order
-      stateSave: true, // Enable state saving
-    });
+    options.order = [[8, "asc"]];
   } else if (
     window.location.pathname === "/controllers/tickets/subordinate_tickets.php"
   ) {
-    table = $(".data-table").DataTable({
-      order: [[11, "asc"]], // Sort by the second column in ascending order
-      // pageLength: 50, // Set the number of rows per page
-      stateSave: true, // Enable state saving
-    });
-  } else {
-    table = $(".data-table").DataTable({
-      paging: true, // Enable pagination
-      // pageLength: 10, // Set the number of rows per page
-      stateSave: true, // Enable state saving
-      ordering: true, // Enable sorting
-      columns: [
-        { width: "5%" },
-        { width: "20%" },
-        { width: "10%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-        { width: "5%" },
-      ],
-      // autoWidth: true, // Disable auto width calculation
-    });
+    options.order = [[11, "asc"]];
   }
+
+  // Initialize the data table library on the table with the class data-table
+  table = $(".data-table").DataTable(options);
 });
 
 // Initialize the data table library on the table with the class search-data-table
@@ -66,7 +59,7 @@ $(document).ready(function () {
   });
 });
 
-// Initialize the data table library on the table with the class search-data-table
+// Initialize the data table library on the table with the class nst (non standard table)
 $(document).ready(function () {
   table = $(".nst").DataTable({
     paging: true, // Enable pagination

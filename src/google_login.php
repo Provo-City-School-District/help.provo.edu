976faa8f0e37ea936f2d645a54074c194df5e2fb
Josh Espinoza <joshespi@gmail.com>
2024-03-22 13:49:02 -0600

    Remove unnecessary line in google_login.php

--- src/google_login.php
+++ src/google_login.php
@@ -104,7 +104,6 @@ if (isset($_GET['code'])) {
             'supervisor_username' => $local_query_data['supervisor_username'],
             'is_location_manager' => $local_query_data['is_location_manager'],
             'location_manager_sitenumber' => $local_query_data['location_manager_sitenumber'],
-
         );
         // Set color scheme
         $_SESSION['color_scheme'] = $local_query_data['color_scheme'];

7dd4c5a1a24f434a68718ede6873aa1d2cdc5ba5
Braxton Anderson <braxtona@provo.edu>
2024-03-26 13:22:47 -0600

    log status change when client closes ticket

--- src/ajax/close_ticket.php
+++ src/ajax/close_ticket.php
@@ -18,7 +18,7 @@ if (!isset($ticket_id)) {
 }
 
 $ticket_client_res = $database->execute_query("SELECT client FROM tickets WHERE tickets.id = ?", [$ticket_id]);
-$ticket_client_data = mysqli_fetch_assoc($ticket_client_res);
+$ticket_client_data = $ticket_client_res->fetch_assoc();
 
 $ticket_client = $ticket_client_data["client"];
 if ($username != $ticket_client) {
@@ -27,7 +27,24 @@ if ($username != $ticket_client) {
     exit;
 }
 
-$res = $database->execute_query("UPDATE tickets SET status = 'closed' WHERE tickets.id = ?", [$ticket_id]);
+$old_ticket_res = $database->execute_query("SELECT * FROM tickets WHERE id = ?", [$ticket_id]);
+$old_ticket_data = $old_ticket_res->fetch_assoc();
+
+$updatedStatus = "closed";
+
+$res = $database->execute_query("UPDATE tickets SET status = ? WHERE tickets.id = ?", [$updatedStatus, $ticket_id]);
+
+if (isset($old_ticket_data['status']) && $old_ticket_data['status'] != $updatedStatus) {
+	logTicketChange($database, $ticket_id, $_SESSION['username'], "status", $old_ticket_data['status'], $updatedStatus);
+	$changesMessage .= "<li>Changed Status from " . $old_ticket_data['status'] . " to " . $updatedStatus . "</li>";
+
+	// Check if the ticket has an alert about not being updated in last 48 hours and clear it since the ticket was just updated.
+	removeAlert($database, $alert48Message, $ticket_id);
+	removeAlert($database, $alert7DayMessage, $ticket_id);
+	removeAlert($database, $alert15DayMessage, $ticket_id);
+	removeAlert($database, $alert20DayMessage, $ticket_id);
+}
+
 if (!$res) {
     log_app(LOG_ERR, "[close_ticket.php] Failed to execute query");
     http_response_code(500);

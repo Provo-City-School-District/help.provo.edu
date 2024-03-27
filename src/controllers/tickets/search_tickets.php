5c5a29e79467e3477326b8331c07f2357290233f
Josh Espinoza <joshespi@gmail.com>
2024-03-27 15:02:10 -0600

    current working file. Tech forms work returning results file. client form does nothing currently

--- src/controllers/tickets/search_tickets.php
+++ src/controllers/tickets/search_tickets.php
@@ -6,162 +6,70 @@ require_once('swdbconnect.php');
 include("ticket_utils.php");
 ?>
 <h1>Search Ticket Information</h1>
-<div class="tabNav">
-    <button class="tablinks" onclick="openTab(event, 'Tickets')">Search Tickets</button>
-    <button class="tablinks" onclick="openTab(event, 'Notes')">Search Notes</button>
-</div>
+<?php
+if (user_is_tech($_SESSION['username'])) {
+    //Technician Search Form
+?>
+    <div class="tabNav">
+        <button class="tablinks" onclick="openTab(event, 'Tickets')">Search Tickets</button>
+        <button class="tablinks" onclick="openTab(event, 'Notes')">Search Notes</button>
+    </div>
 
-<div id="Tickets" class="tabcontent">
-    <h2>Search Tickets</h2>
-    <form method="get" action="" id="searchTickets">
-        <div class="form-group">
-            <label for="search_id">Ticket ID:</label>
-            <input type="number" class="form-control" id="search_id" name="search_id" value="<?php echo htmlspecialchars($search_id); ?>">
-        </div>
-        <div class="form-group">
-            <label for="search_name">Keywords:</label>
-            <input type="text" class="form-control" id="search_name" name="search_name" value="<?php echo htmlspecialchars($search_name); ?>">
-        </div>
-        <div class="form-group">
-            <label for="priority">Priority:</label>
-            <select id="priority" name="priority">
-                <option value="" selected></option>
+    <div id="Tickets" class="tabcontent">
+        <h2>Search Tickets</h2>
+        <?php include __DIR__ . '/../../includes/templates/search_tickets_tech_form.php'; ?>
+    </div>
 
-                <option value="1" <?= ($search_priority == '1') ? ' selected' : '' ?>>Critical</option>
-                <option value="3" <?= ($search_priority == '3') ? ' selected' : '' ?>>Urgent</option>
-                <option value="5" <?= ($search_priority == '5') ? ' selected' : '' ?>>High</option>
-                <option value="10" <?= ($search_priority == '10') ? ' selected' : '' ?>>Standard</option>
-                <option value="15" <?= ($search_priority == '15') ? ' selected' : '' ?>>Client Response</option>
-                <option value="30" <?= ($search_priority == '30') ? ' selected' : '' ?>>Project</option>
-                <option value="60" <?= ($search_priority == '60') ? ' selected' : '' ?>>Meeting Support</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="search_location">Department/Location:</label>
-            <!-- <input type="text" class="form-control" id="search_location" name="search_location" value="<?php echo htmlspecialchars($search_location); ?>"> -->
-            <select id="search_location" name="search_location">
-                <option value="" selected></option>
-                <?php
-                // Query the locations table to get the departments
-                $department_query = "SELECT * FROM locations WHERE is_department = TRUE ORDER BY location_name ASC";
-                $department_result = $database->execute_query($department_query);
+    <div id="Notes" class="tabcontent">
+        <h2>Search Notes</h2>
+        <?php include __DIR__ . '/../../includes/templates/search_notes_form.php'; ?>
+    </div>
+<?php
+} //End Technician Search Forms
+else {
+    //Non-Technician Search Form
+    include __DIR__ . '/../../includes/templates/search_tickets_client_form.php';
+}
+?>
 
-                // Create a "Department" optgroup and create an option for each department
-                echo '<optgroup label="Department">';
-                while ($locations = mysqli_fetch_assoc($department_result)) {
-                    $selected = '';
-                    if (isset($search_location) && $locations['sitenumber'] == $search_location) {
-                        $selected = 'selected';
-                    }
-                    echo '<option value="' . $locations['sitenumber'] . '" ' . $selected . '>' . $locations['location_name'] . '</option>';
-                }
-                echo '</optgroup>';
-
-                // Query the locations table to get the locations
-                $location_query = "SELECT * FROM locations WHERE is_department = FALSE ORDER BY location_name ASC";
-                $location_result = $database->execute_query($location_query);
-
-                // Create a "Location" optgroup and create an option for each location
-                echo '<optgroup label="Location">';
-                while ($locations = mysqli_fetch_assoc($location_result)) {
-                    $selected = '';
-                    if (isset($search_location) && $locations['sitenumber'] == $search_location) {
-                        $selected = 'selected';
-                    }
-                    echo '<option value="' . $locations['sitenumber'] . '" ' . $selected . '>' . $locations['location_name'] . '</option>';
-                }
-                echo '</optgroup>';
-                ?>
-            </select>
-        </div>
-        <?= $search_employee ?>
-        <div class="form-group">
-            <label for="search_employee">Tech:</label>
-            <!-- <input type="text" class="form-control" id="search_employee" name="search_employee" value="<?php echo htmlspecialchars($search_employee); ?>"> -->
-            <select id="search_employee" name="search_employee">
-                <option value="" selected></option>
-                <option value="Unassigned" <?= $search_employee == 'helpdesk' ? 'selected' : '' ?>>Unassigned</option>
-                <?php foreach ($techusernames as $techuser) : ?>
-                    <option value="<?= $techuser ?>" <?= $search_employee === $techuser ? 'selected' : '' ?>><?= $techuser ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="search_client">Client:</label>
-            <input type="text" class="form-control" id="search_client" name="search_client" value="<?php echo htmlspecialchars($search_client); ?>">
-        </div>
-        <div class="form-group">
-            <label for="search_status">Status:</label>
-            <select id="status" name="search_status">
-                <option value="" selected></option>
-                <option value="open" <?= ($search_status == 'open' || $search_status == 1) ? ' selected' : '' ?>>Open</option>
-                <option value="closed" <?= ($search_status == 'closed' || $search_status == 3) ? ' selected' : '' ?>>Closed</option>
-                <option value="resolved" <?= ($search_status == 'resolved' || $search_status == 5) ? ' selected' : '' ?>>Resolved</option>
-                <option value="pending" <?= ($search_status == 'pending' || $search_status == 7) ? ' selected' : '' ?>>Pending</option>
-                <option value="vendor" <?= ($search_status == 'vendor' || $search_status == 12) ? ' selected' : '' ?>>Vendor</option>
-                <option value="maintenance" <?= ($search_status == 'maintenance' || $search_status == 11) ? ' selected' : '' ?>>Maintenance</option>
-            </select>
-        </div>
-
-        <div>
-
-            <label for="start_date">Start Date:</label>
-            <input type="date" id="start_date" name="start_date" value="<?php echo isset($_GET['start_date']) ? $_GET['start_date'] : ''; ?>">
-
-            <label for="end_date">End Date:</label>
-            <input type="date" id="end_date" name="end_date" value="<?php echo isset($_GET['end_date']) ? $_GET['end_date'] : ''; ?>">
-
-            <input type="checkbox" name="dates[]" value="created" <?php echo (isset($_GET['dates']) && in_array('created', $_GET['dates'])) ? 'checked' : ''; ?>> Created Date
-            <input type="checkbox" name="dates[]" value="last_updated" <?php echo (isset($_GET['dates']) && in_array('last_updated', $_GET['dates'])) ? 'checked' : ''; ?>> Last Updated
-            <input type="checkbox" name="dates[]" value="due_date" <?php echo (isset($_GET['dates']) && in_array('due_date', $_GET['dates'])) ? 'checked' : ''; ?>> Due Date
-        </div>
-        <div>
-            <label for="search_name">Search Archived Tickets:</label>
-            <input type="checkbox" id="search_archived" name="search_archived" value="1" <?php echo (isset($_GET['search_archived']) == 1) ? 'checked' : ''; ?>>
-        </div>
-        <button type="submit" class="btn btn-primary">Search</button>
-
-        <button type="reset" id="resetBtn" class="btn btn-secondary">Reset</button>
-    </form>
-</div>
-
-<div id="Notes" class="tabcontent">
-    <h2>Search Notes</h2>
-    <form method="get" action="" id="searchNotes">
-        <div class="form-group">
-            <label for="search_noteid">Note ID:</label>
-            <input type="number" class="form-control" id="search_noteid" name="search_noteid" value="<?php echo htmlspecialchars($search_id); ?>">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Search</button>
-
-        <button type="reset" id="resetBtn" class="btn btn-secondary">Reset</button>
-    </form>
-</div>
 
 <div id="results">
 
 </div>
+
+
+
+
 <script>
-    // tab behavior
     function openTab(evt, tabName) {
         var i, tabcontent, tablinks;
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
+            if (tabcontent[i]) {
+                tabcontent[i].style.display = "none";
+            }
         }
         tablinks = document.getElementsByClassName("tablinks");
         for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
+            if (tablinks[i]) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
         }
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " active";
+        var tab = document.getElementById(tabName);
+        if (tab) {
+            tab.style.display = "block";
+        }
+        if (evt.currentTarget) {
+            evt.currentTarget.className += " active";
+        }
     }
+
     // Handle Search Form Submission
     function handleSubmit(formId, urlPath) {
-        document.getElementById(formId).addEventListener('submit', function(e) {
+        document.getElementById(formId).addEventListener("submit", function(e) {
             e.preventDefault();
-
+            console.log(formId);
+            console.log(urlPath);
             var url = new URL(urlPath, window.location.origin);
             var formData = new FormData(this);
             var params = new URLSearchParams(formData);
@@ -169,22 +77,24 @@ include("ticket_utils.php");
             url.search = params;
 
             fetch(url, {
-                    method: 'GET'
+                    method: "GET",
                 })
-                .then(response => response.text())
-                .then(data => {
+                .then((response) => response.text())
+                .then((data) => {
                     // Display the data in your page
-                    document.getElementById('results').innerHTML = data;
+                    document.getElementById("results").innerHTML = data;
                 })
-                .catch(error => {
-                    console.error('Error:', error);
+                .catch((error) => {
+                    console.error("Error:", error);
                 });
         });
     }
-
-    handleSubmit('searchTickets', 'includes/search_tickets_query_builder.php');
-    handleSubmit('searchNotes', 'includes/search_notes_query_builder.php');
+    handleSubmit("searchTickets", "includes/search_tickets_tech_query_builder.php");
+    handleSubmit("searchNotes", "includes/search_notes_query_builder.php");
+    handleSubmit("searchClient", "includes/search_tickets_client_query_builder.php");
 </script>
+
+
 
 <script src="/includes/js/pages/search_tickets.js?v=1.0.0" type="text/javascript"></script>
 <?php include("footer.php"); ?>

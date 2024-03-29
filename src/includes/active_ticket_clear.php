ee1da3fb95bd4146c668858946ce91a366d94cd5
Braxton Anderson <braxtona@provo.edu>
2024-03-29 10:36:18 -0600

    fix serializer logging error

--- src/includes/active_ticket_clear.php
+++ src/includes/active_ticket_clear.php
@@ -2,8 +2,9 @@
 require_once('helpdbconnect.php');
 require_once('ticket_utils.php');
 
-$res = $database->execute_query("UPDATE users SET active_ticket = NULL, active_ticket_updated = NULL WHERE active_ticket_updated < DATE_SUB(NOW(), INTERVAL 30 MINUTE)");
+$res = $database->execute_query("UPDATE users SET active_ticket = NULL, active_ticket_updated = NULL WHERE active_ticket_updated < DATE_SUB(NOW(), INTERVAL 1 MINUTE)");
 if (!$res) {
 	log_app(LOG_ERR, "[active_ticket_clear.php] Failed to run query to clear active_ticket and active_ticket_updated");
+} else {
+	log_app(LOG_INFO, "[active_ticket_clear.php] Succesfully ran active_ticket clear query");
 }
-

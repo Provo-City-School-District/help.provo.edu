1e958791b35be775dab688a6be47689e66c13aef
Braxton Anderson <braxtona@provo.edu>
2024-04-16 15:20:05 -0600

    create basic tests.php

---
+++ src/tests.php
@@ -0,0 +1,45 @@
+<?php
+require "block_file.php";
+require "ticket_utils.php";
+require "helpdbconnect.php";
+
+$username = $_SESSION["username"];
+// Test creating ticket
+$ticket_id = 0;
+create_ticket($username, "Ticket Test", "Help", "", 408, $ticket_id);
+
+assert($ticket_id !== 0);
+
+// Test reading from ticket
+$res = $database->execute_query("SELECT * FROM help.tickets WHERE id = ?", [$ticket_id]);
+
+assert($res);
+assert($res->num_rows === 1);
+
+while ($row = $res->fetch_assoc()) {
+	assert($row["name"] === "Ticket Test");
+	assert($row["description"] === "Help");
+}
+
+echo "Ticket creation: passed<br>";
+
+// Add a note on the ticket
+$res = add_note_with_filters($ticket_id, $username, "Note test", 1, 32, 0, 14, true);
+assert($res);
+
+// Test reading from note
+$res = $database->execute_query("SELECT * FROM help.notes WHERE linked_id = ?", [$ticket_id]);
+
+assert($res);
+assert($res->num_rows === 1);
+
+while ($row = $res->fetch_assoc()) {
+	assert($row["creator"] === $username);
+	assert($row["note"] === "Note test");
+	assert($row["work_hours"] === 1);
+	assert($row["work_minutes"] === 32);
+	assert($row["travel_hours"] === 0);
+	assert($row["travel_minutes"] === 14);
+}
+
+echo "Note creation: passed";

fc93895ebc35da429220c85bab70cd5712a441d1
Braxton Anderson <braxtona@provo.edu>
2024-04-18 14:47:29 -0600

    update datatables to 2.0.5

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -24,7 +24,7 @@
 <script src="/includes/js/jquery-3.7.1.min.js" type="text/javascript"></script>
 <script src="/includes/js/jquery-ui.min.js" type="text/javascript"></script>
 <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
-<script src="/includes/js/dataTables-1.13.7/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="/includes/js/dataTables-2.0.5/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="/vendor/tinymce/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
     var userPref = '<?php echo isset($_SESSION['color_scheme']) ? $_SESSION['color_scheme'] : 'light'; ?>';

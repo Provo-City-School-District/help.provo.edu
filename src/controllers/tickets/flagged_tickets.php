91d7b5b5f58ebbaeb126fff0186bb7af60a1c103
Braxton Anderson <braxtona@provo.edu>
2024-04-18 09:31:08 -0600

    migrate flagged_tickets to twig

--- src/controllers/tickets/flagged_tickets.php
+++ src/controllers/tickets/flagged_tickets.php
@@ -1,41 +1,50 @@
 <?php
-require_once("block_file.php");
-require_once(from_root("/includes/tickets_template.php"));
-include("header.php");
+require from_root("/vendor/autoload.php");
+require from_root("/new-controllers/ticket_base_variables.php");
+require "ticket_utils.php";
 
-if ($_SESSION['permissions']['is_admin'] != 1) {
-    // User is not an admin
-    if ($_SESSION['permissions']['can_view_tickets'] == 0) {
-        // User does not have permission to view tickets
-        echo 'You do not have permission to view tickets.';
-        exit;
-    }
-}
-require_once('helpdbconnect.php');
-include("ticket_utils.php");
-//page query
-$username = $_SESSION['username'];
+session_start();
+
+$loader = new \Twig\Loader\FilesystemLoader(from_root('/views'));
+$twig = new \Twig\Environment($loader, [
+    'cache' => from_root('/twig-cache')
+]);
+
 $ticket_query = <<<STR
-        SELECT tickets.*, GROUP_CONCAT(DISTINCT alerts.alert_level) AS alert_levels
-        FROM tickets
-        LEFT JOIN alerts ON tickets.id = alerts.ticket_id
-        WHERE tickets.id IN (
-            SELECT flagged_tickets.ticket_id 
-            FROM flagged_tickets 
-            WHERE flagged_tickets.user_id IN (
-                SELECT users.id 
-                FROM users 
-                WHERE users.username = ?
-            )
-        )
-        GROUP BY tickets.id
-        STR;
+	SELECT tickets.*, GROUP_CONCAT(DISTINCT alerts.alert_level) AS alert_levels
+	FROM tickets
+	LEFT JOIN alerts ON tickets.id = alerts.ticket_id
+	WHERE tickets.id IN (
+		SELECT flagged_tickets.ticket_id 
+		FROM flagged_tickets 
+		WHERE flagged_tickets.user_id IN (
+			SELECT users.id 
+			FROM users 
+			WHERE users.username = ?
+		)
+	)
+	GROUP BY tickets.id
+STR;
+
 $ticket_result = $database->execute_query($ticket_query, [$username]);
+$tickets = get_parsed_ticket_data($ticket_result);
 
-?>
+echo $twig->render('flagged_tickets.twig', [
+    // base variables
+    'color_scheme' => $color_scheme,
+    'current_year' => $current_year,
+    'user_permissions' => $permissions,
+    'wo_time' => $wo_time,
+    'user_pref' => $user_pref,
+    'ticket_limit' => $ticket_limit,
+	'status_alert_type' => $status_alert_type,
+	'status_alert_message' => $status_alert_message,
 
-<h1>Flagged Tickets</h1>
+    // ticket_base variables
+    'subord_count' => $subord_count,
+	'num_assigned_tickets' => $num_assigned_tickets,
+	'num_flagged_tickets' => $num_flagged_tickets,
 
-<?php display_tickets_table($ticket_result, $database); ?>
-
-<?php include("footer.php"); ?>
+	// ticket_table_base variables
+	'tickets' => $tickets
+]);

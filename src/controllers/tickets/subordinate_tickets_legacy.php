e72c057a19ea84e3bb8f156ab57a234d9c55d521
Braxton Anderson <braxtona@provo.edu>
2024-04-19 13:59:52 -0600

    migrate subordinate tickets view to twig

---
+++ src/controllers/tickets/subordinate_tickets_legacy.php
@@ -0,0 +1,50 @@
+<?php
+include("header.php");
+
+if ($_SESSION['permissions']['is_admin'] != 1) {
+    // User is not an admin
+    if ($_SESSION['permissions']['is_supervisor'] == 0) {
+        // User does not have permission to view tickets
+        echo 'You do not have permission to view tickets.';
+        exit;
+    }
+}
+require_once('helpdbconnect.php');
+require_once(from_root("/includes/block_file.php"));
+require_once(from_root("/includes/ticket_utils.php"));
+require_once(from_root("/includes/tickets_template.php"));
+require_once(from_root("/includes/alerts_template.php"));
+
+$username = $_SESSION['username'];
+
+if ($_SESSION['permissions']['is_supervisor'] == 1) {
+    //Tickets query
+    $ticket_query = <<<STR
+    SELECT tickets.*, GROUP_CONCAT(DISTINCT alerts.alert_level) AS alert_levels
+    FROM tickets
+    JOIN users ON tickets.employee = users.username
+    LEFT JOIN alerts ON tickets.id = alerts.ticket_id
+    WHERE users.supervisor_username = ?
+    AND tickets.status NOT IN ('closed', 'resolved')
+    GROUP BY tickets.id
+    ORDER BY tickets.last_updated DESC
+    STR;
+
+    $ticket_result = $database->execute_query($ticket_query, [$username]);
+
+    //Alerts query
+    $alerts_query = <<<STR
+    SELECT alerts.* 
+    FROM alerts 
+    JOIN users ON alerts.employee = users.username
+    WHERE users.supervisor_username = ?
+    AND alerts.supervisor_alert IN (0, 1)
+    STR;
+
+    $alerts_result = $database->execute_query($alerts_query, [$username]);
+
+    echo ' <h1>Subordinate Tickets</h1>';
+    display_ticket_alerts($alerts_result);
+    display_tickets_table($ticket_result, $database);
+}
+include("footer.php");

47d714a257762c48b8b8249da4a53cb263f0ca82
Braxton Anderson <braxtona@provo.edu>
2024-04-25 13:50:40 -0600

    add another check in upload viewer (only gets basename of file)

--- src/public/upload_viewer.php
+++ src/public/upload_viewer.php
@@ -1,7 +1,8 @@
 <?php
 require "block_file.php";
 
-$file_path = $_GET["file"];
+$file_path = basename($_GET["file"]);
+
 $real_user_path = realpath(from_root("/../uploads/$file_path"));
 $real_base_path = realpath(from_root("/../uploads/"));
 
@@ -10,6 +11,7 @@ if ($real_user_path === false || (substr($real_user_path, 0, strlen($real_base_p
 	echo "Error parsing file request";
 	exit;
 }
+
 $content_type = mime_content_type($real_user_path);
 $content_size = filesize($real_user_path);
 

840d0f98161ab226b84084c2c49a053cbc21394e
Braxton Anderson <braxtona@provo.edu>
2024-04-25 14:34:39 -0600

    improve upload_viewer error handling

--- src/public/upload_viewer.php
+++ src/public/upload_viewer.php
@@ -7,12 +7,17 @@ $real_user_path = realpath(from_root("/../uploads/$file_path"));
 $real_base_path = realpath(from_root("/../uploads/"));
 
 
+// Validate that the file is being accessed in ${PROJECT_ROOT}/uploads
 if ($real_user_path === false || (substr($real_user_path, 0, strlen($real_base_path)) != $real_base_path)) {
 	echo "Error parsing file request";
 	exit;
 }
 
 $content_type = mime_content_type($real_user_path);
+if (!$content_type) {
+	echo "Failed to get file type";
+}
+
 $content_size = filesize($real_user_path);
 
 header("Content-Type: $content_type");

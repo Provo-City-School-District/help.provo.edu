ce7badc367372f03fa9158b68c6f2dc4677a915a
Braxton Anderson <braxtona@provo.edu>
2024-05-09 12:44:16 -0600

    remove username from data table view

--- src/php-includes/tickets_template.php
+++ src/php-includes/tickets_template.php
@@ -92,7 +92,7 @@ function display_tickets_table($tickets, $database)
             <td data-cell="ID"><a href="/controllers/tickets/edit_ticket.php?id=' . $ticket["id"] . '">' . $ticket["id"] . '</a></td>
             <td class="details" data-cell="Request Detail"><a href="/controllers/tickets/edit_ticket.php?id=' . $ticket["id"] . '">' . $ticket["name"] . ':</a>' . limitChars($descriptionWithoutLinks, 100) . '</td>
             <td class="latestNote" data-cell="Latest Note:">' . limitChars($latest_note_str, 150) . '</td>
-            <td data-cell="Client: ">' . $clientFirstName . " " . $clientLastName . " (" . $ticket['client'] . ")" . '</td>
+            <td data-cell="Client: ">' . $clientFirstName . " " . $clientLastName . '</td>
             <td data-cell="Location">' .
             (!empty($location_name) ? $location_name . '<br><br>' : '') .
             (!empty($ticket['room']) ? 'RM ' . $ticket['room'] : '') .

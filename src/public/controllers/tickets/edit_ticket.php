16539e0ea0c05590d7c22a564edf03f22e768ee1
Braxton Anderson <braxtona@provo.edu>
2024-05-23 08:09:35 -0600

    update version for note_submit.js

--- src/public/controllers/tickets/edit_ticket.php
+++ src/public/controllers/tickets/edit_ticket.php
@@ -1261,7 +1261,7 @@ if (strtolower($ticket["employee"]) == strtolower($username)) {
         setInterval(updateTimeSinceLastNote, 60000); // 60000 milliseconds
     </script>
 <?php endif; ?>
-<script src="/includes/js/note_submit.js?v=?v=1.0.04" type="text/javascript"></script>
+<script src="/includes/js/note_submit.js?v=?v=1.0.05" type="text/javascript"></script>
 <script src="/includes/js/pages/edit_ticket.js?v=1.0.05" type="text/javascript"></script>
 <?php include("footer.php"); ?>
 

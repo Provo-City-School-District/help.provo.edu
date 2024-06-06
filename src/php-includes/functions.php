644bfe47dbda8b14ca92a7551dc00a6f143538c1
Josh Espinoza <joshespi@gmail.com>
2024-06-06 10:39:08 -0600

    notes about potential duplicate function

--- src/php-includes/functions.php
+++ src/php-includes/functions.php
@@ -321,8 +321,7 @@ function calculateTimeSinceLastLogin()
     // Return null if last_login is not set in the session
     return null;
 }
-
-
+//TODO: have session_is_tech and user_is_tech which might be used as the same thing
 function user_is_tech(string $username)
 {
     $userPermissionsResult = HelpDB::get()->execute_query("SELECT is_tech FROM users WHERE username = ?", [$username]);

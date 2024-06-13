6d86a153f51a3443bf4a91603e755994715a6c3b
Josh Espinoza <joshespi@gmail.com>
2024-06-13 13:36:51 -0600

    disabling alert for now. not currently breaking sessions

--- src/php-includes/footer.php
+++ src/php-includes/footer.php
@@ -7,14 +7,14 @@
         <h2>Inactivity Alert</h2>
         <p>You've been inactive for more than 30 minutes.</p>
         <?php
-        $time_difference = calculateTimeSinceLastLogin();
-        // Check if the user has been logged in for more than 2 hours and display the appropriate message about session likely broken
-        if ($time_difference > 2 * 60 * 60) {
-            echo "<p>Your session may have expired. It's recommended to log out and back in.</p>";
-            if (isset($_SESSION['username'])) {
-                log_app(LOG_INFO, $_SESSION['username'] . " alerted of old session");
-            }
-        }
+        // $time_difference = calculateTimeSinceLastLogin();
+        // // Check if the user has been logged in for more than 2 hours and display the appropriate message about session likely broken
+        // if ($time_difference > 3 * 60 * 60) {
+        //     echo "<p>Your session may have expired. It's recommended to log out and back in.</p>";
+        //     if (isset($_SESSION['username'])) {
+        //         log_app(LOG_INFO, $_SESSION['username'] . " alerted of old session");
+        //     }
+        // }
         ?>
         <button onclick="dismiss_timeout_modal()">Dismiss</button>
         <button onclick="location.reload()">Reload Page</button>

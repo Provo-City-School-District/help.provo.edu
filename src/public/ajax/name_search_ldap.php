8ec6ef1e5d42ac8e005279071190ad2f50b98458
Braxton Anderson <braxtona@provo.edu>
2024-07-08 13:11:32 -0600

    add support for archived employee client search

--- src/public/ajax/name_search_ldap.php
+++ src/public/ajax/name_search_ldap.php
@@ -11,6 +11,7 @@ log_app(LOG_INFO, "input: ".$input);
 $ldap_host = getenv('LDAPHOST');
 $ldap_port = getenv('LDAPPORT');
 $ldap_dn = getenv('LDAP_DN');
+$ldap_archived_dn = getenv('LDAP_ARCHIVED_DN');
 $ldap_user = getenv('LDAP_USER');
 $ldap_password = getenv('LDAP_PASS');
 
@@ -39,6 +40,9 @@ if (count($input_split) == 2) {
 $ldap_result = ldap_search($ldap_conn, $ldap_dn, $search);
 $entries = ldap_get_entries($ldap_conn, $ldap_result);
 
+$ldap_result_archived = ldap_search($ldap_conn, $ldap_archived_dn, $search);
+$entries_archived = ldap_get_entries($ldap_conn, $ldap_result_archived);
+
 $results = [];
 for ($i = 0; $i < $entries['count']; $i++) {
     $samaccountname = $entries[$i]['samaccountname'][0] ?: null;
@@ -55,6 +59,23 @@ for ($i = 0; $i < $entries['count']; $i++) {
         $location_code = 1896;
 
     $results[] = ['username' => $samaccountname, 'firstName' => $firstname, 'lastName' => $lastname, 'location' => location_name_from_id($location_code)];
+}
+
+for ($i = 0; $i < $entries_archived['count']; $i++) {
+    $samaccountname = $entries_archived[$i]['samaccountname'][0] ?: null;
+    $firstname = $entries_archived[$i]['givenname'][0] ?: null;
+    $lastname = $entries_archived[$i]['sn'][0] ?: null;
+
+    $location_code = 38;
+    if (array_key_exists("ou", $entries_archived[$i])) {
+        $location_code = intval($entries_archived[$i]['ou'][0] ?: 38);
+    }
+
+    // Hacky mapping for aux services, should be 1896 internally
+    if ($location_code == 1892)
+        $location_code = 1896;
+
+    $results[] = ['username' => $samaccountname, 'firstName' => $firstname, 'lastName' => $lastname, 'location' => "inactive"];
 }
 
 header('Content-Type: application/json');

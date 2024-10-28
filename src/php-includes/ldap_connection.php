bff89dc92b39dd117b9e30bf3cc50d6f9c381264
Braxton Anderson <braxtona@provo.edu>
2024-10-28 08:22:38 -0600

    fix ldaps

--- src/php-includes/ldap_connection.php
+++ src/php-includes/ldap_connection.php
@@ -3,7 +3,7 @@
 function get_ldaps_conn()
 {
     ldap_set_option(null, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_DEMAND);
-   // ldap_set_option(null, LDAP_OPT_DEBUG_LEVEL, 7);
+    //ldap_set_option(null, LDAP_OPT_DEBUG_LEVEL, 7);
     ldap_set_option(null, LDAP_OPT_PROTOCOL_VERSION, 3);
     ldap_set_option(null, LDAP_OPT_X_TLS_CACERTDIR, "/etc/ssl/certs");
 

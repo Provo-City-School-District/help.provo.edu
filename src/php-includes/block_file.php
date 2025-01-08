c775e634f30642385bd2e3d8f576918bcc109a09
Josh Espinoza <joshespi@gmail.com>
2025-01-08 09:38:01 -0700

    refactor session check to ensure 'REQUEST_URI' exists before storing

--- src/php-includes/block_file.php
+++ src/php-includes/block_file.php
@@ -3,13 +3,14 @@ if (!session_id()) {
     session_start();
 }
 
-// check if logged in. redirects to login page if not
-if (!$_SESSION['username']) {
-    // Store the requested page in the session
-    $_SESSION['requested_page'] = $_SERVER['REQUEST_URI'];
+// Check if 'username' exists in the session
+if (empty($_SESSION['username'])) {
+    // Store the requested page in the session, checking if 'REQUEST_URI' exists
+    if (isset($_SERVER['REQUEST_URI'])) {
+        $_SESSION['requested_page'] = $_SERVER['REQUEST_URI'];
+    }
 
+    // Redirect to the login page
     header('Location:' . getenv('ROOTDOMAIN'));
     exit;
 }
-
-?>

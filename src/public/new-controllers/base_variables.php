7e28a1c894ff5dcad4ec81cbf78b452513fb50e2
Josh Espinoza <joshespi@gmail.com>
2025-01-29 06:25:40 -0700

    add location manager permission to base_variables

--- src/public/new-controllers/base_variables.php
+++ src/public/new-controllers/base_variables.php
@@ -22,7 +22,8 @@ $permissions = [
     "is_supervisor" => $_SESSION["permissions"]["is_supervisor"] != 0,
     "is_admin" => $_SESSION["permissions"]["is_admin"] != 0,
     "is_tech" => $_SESSION["permissions"]["is_tech"] != 0,
-    "is_intern" => $_SESSION["permissions"]["is_intern"] != 0
+    "is_intern" => $_SESSION["permissions"]["is_intern"] != 0,
+    "is_location_manager" => $_SESSION["permissions"]["is_location_manager"] != 0,
 ];
 
 

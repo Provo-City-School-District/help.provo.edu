18f0ee0643f8f30f87e60692f795971745b7efdb
Josh Espinoza <joshespi@gmail.com>
2025-04-28 09:46:37 -0600

    prettier formatting

--- src/public/api/v1/create_ticket.php
+++ src/public/api/v1/create_ticket.php
@@ -3,10 +3,12 @@ require "api_authenticate.php";
 require "ticket_utils.php";
 
 // make sure ticket title and ticket description are set
-if (!isset($_POST["ticket_title"]) || 
+if (
+    !isset($_POST["ticket_title"]) ||
     !isset($_POST["ticket_description"]) ||
     !isset($_POST["ticket_department"]) ||
-    !isset($_POST["ticket_location"])) {
+    !isset($_POST["ticket_location"])
+) {
     http_response_code(400);
     exit;
 }

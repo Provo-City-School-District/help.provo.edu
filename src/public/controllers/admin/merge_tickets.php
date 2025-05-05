3429d193845fb096d092a92b852e25af37652512
Josh Espinoza <joshespi@gmail.com>
2025-05-05 14:03:40 -0600

    add developer role check to merge tickets page

--- src/public/controllers/admin/merge_tickets.php
+++ src/public/controllers/admin/merge_tickets.php
@@ -9,6 +9,7 @@ if ($_SESSION['permissions']['is_admin'] != 1) {
     exit;
 }
 
+$is_developer = get_user_setting(get_id_for_user($_SESSION['username']), "is_developer") ?? 0;
 
 $loader = new \Twig\Loader\FilesystemLoader(from_root('/../views'));
 $twig = new \Twig\Environment($loader, [
@@ -40,19 +41,6 @@ echo $twig->render('merge_ticket.twig', [
     'app_version' => $app_version,
 
     // Page Variables
-    // 'exclude_days' => $exclude_parsed,
     'username' => $_SESSION['username'],
-    // 'user_result' => $users,
-    // 'user_id' => $user_id,
-    // 'employee_id' => $employee_id,
-    // 'username' => $username,
-    // 'first_name' => $firstname,
-    // 'last_name' => $lastname,
-    // 'email' => $email,
-    // // 'note_order' => $note_order,
-    // 'hide_alerts' => $hide_alerts,
-    // 'user_times' => $user_times,
-    // 'user_time_total' => $user_time_total,
-    // 'note_count' => $note_count,
-    // 'show_alerts' => $show_alerts
+    'is_developer' => $is_developer,
 ]);

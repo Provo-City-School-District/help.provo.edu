6f510ac657c665ba66a2dc060132b209f75da102
Josh Espinoza <joshespi@gmail.com>
2025-05-22 14:39:21 -0600

    added delete controller

---
+++ src/public/controllers/tickets/delete_workflow_template.php
@@ -0,0 +1,17 @@
+<?php
+require "block_file.php";
+require_once("helpdbconnect.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['step_id'])) {
+    $template_id = intval($_GET['step_id']);
+
+    $delete_query = HelpDB::get()->execute_query("DELETE FROM workflow_templates WHERE id = ?", [$template_id]);
+
+    log_app(LOG_INFO, "[delete_workflow_template.php] Deleted workflow template with ID: $template_id");
+
+    header('Location: manage_workflow_template.php');
+    exit;
+} else {
+    http_response_code(400);
+    echo "Invalid request.";
+}

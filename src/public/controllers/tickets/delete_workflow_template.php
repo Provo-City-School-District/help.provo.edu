7fa0f1466d4dbf36269fb99d851e4413c7c854fa
Josh Espinoza <joshespi@gmail.com>
2025-05-28 07:55:16 -0600

    switch controller to post requests

--- src/public/controllers/tickets/delete_workflow_template.php
+++ src/public/controllers/tickets/delete_workflow_template.php
@@ -2,8 +2,8 @@
 require "block_file.php";
 require_once("helpdbconnect.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['step_id'])) {
-    $template_id = intval($_GET['step_id']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['step_id'])) {
+    $template_id = intval($_POST['step_id']);
 
     $delete_query = HelpDB::get()->execute_query("DELETE FROM workflow_templates WHERE id = ?", [$template_id]);
 
